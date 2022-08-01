763377e0e6996f22d10ef3c9c366b31250ba489c
hbs30832 <hbs9312@gmail.com>
2022-08-01 13:33:00 +0900

    jquery 슬라이더 및 모달 구현

--- js/front-end/index_jquery.js
+++ js/front-end/index_jquery.js
@@ -6,3 +6,26 @@ $(".btnSide").click(function () {
 $(window).resize(function () {
   $("#gnb").removeClass("on");
 });
+
+// 슬라이드 개수만큼 버튼 생성 및 슬라이드 기능 구현
+let index = 0;
+for (let i = 0; i < $(".slideContainer li").length; i++) {
+  let liElem = `<button data-silde-num=${i}>${i + 1}</button>`;
+  $(".pagination").append(liElem);
+}
+
+$(".slideContainer ul").css(
+  "width",
+  `${$(".slideContainer li").length * 100}vw`
+);
+
+$(".pagination button").click(function () {
+  let slideNum = parseInt($(this).attr("data-silde-num"));
+  $(".slideContainer ul").css("transform", `translate(-${slideNum * 100}vw)`);
+});
+
+$(".modal .btnClose").click(function () {
+  // 페이드 아웃 효과 적용
+  $(".modalBg").fadeOut();
+  // .hide() : display : none;
+});
