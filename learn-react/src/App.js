9f54c65517793c0793dc90d2acfafe023b89c4e0
hbs30832 <hbs9312@gmail.com>
2022-08-05 15:48:59 +0900

    투두 리스트 완성

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import { Reset } from "styled-reset";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <>
       <Reset />
       <Header />
+      <Slider />
     </>
   );
 }
