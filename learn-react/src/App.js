e0f4da592e6607959f1e5a3110af1f6b2c74ce17
hyunjungl <dbswo222@hanmail.net>
2022-08-08 11:01:19 +0900

    changes

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,10 +1,15 @@
+import Header from "./components/Header";
+import { Reset } from "styled-reset";
+import Slider from "./components/Slider";
 
-function countFriend(list) {
-  console.log("친한 친구 세는 중");
-  return list.filter((user) => user.isFriend).length;
+function App() {
+  return (
+    <>
+      <Reset />
+      <Header />
+      <Slider />
+    </>
+  );
 }
-
-
-
 
 export default App;
