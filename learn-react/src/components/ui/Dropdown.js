e03363c6b8fb88efcdfaaf7fe779dcffa5d587d3
hyunjungl <dbswo222@hanmail.net>
2022-08-09 12:35:21 +0900

    Update Dropdown.js

--- learn-react/src/components/ui/Dropdown.js
+++ learn-react/src/components/ui/Dropdown.js
@@ -1,3 +1,40 @@
-export default function Dropdown(){
+import {useState} from "react";
+import styled, {css} from "styled-components";
 
+export default function Dropdown(){
+  const [active, setActive]= useState(false);
+  return (
+<Block active={active}>
+  <ValueBox onClick={()=> setActive(!active)}>메뉴1</ValueBox>
+  
+  <ul>
+    <li>메뉴1</li>
+    <li>메뉴2</li>
+    <li>메뉴3</li>
+  </ul>
+</Block>
+  );
 }
+
+const Block = styled.div`
+transition: 0.25s;
+width: 100px;
+ul {
+  display: none;
+  border: 1px solid #ddd;
+  li{
+    padding: 10px;
+    &:hover {
+      background-color: #ccc;
+    }
+  }
+}
+${({active}) => active && css`
+display:block;
+`}
+`;
+
+const ValueBox = styled.div`
+cursor:pointer;
+user-select: none;
+`;
