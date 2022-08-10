646d75e352b53b3729d2c3f9240c69225cfce6ae
hyunjungl <dbswo222@hanmail.net>
2022-08-10 18:28:33 +0900

    tutorial-contents & changes

--- wordbook/src/App.js
+++ wordbook/src/App.js
@@ -1,20 +1,28 @@
 import WordBook from "./components/WordBook";
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from "styled-components";
+import GrandFather, {
+  GrandContext,
+} from "./components/tutorial-context/GrandFather";
+import Last from "./components/tutorial-context/Last";
 
 const GlobalStyle = createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    margin : 0;
+    padding : 0;
+    box-sizing : border-box;
   }
   li {
-    list-style: :none;
+    list-style: none;
   }
 `;
 
 function App() {
-  return <div>
-    <WordBook/>
-  </div>;
+  return (
+    <div>
+      <GlobalStyle />
+      <WordBook />
+    </div>
+  );
 }
+
 export default App;
--- wordbook/src/components/WordList.js
+++ wordbook/src/components/WordList.js
@@ -1,51 +1,88 @@
 import { useState } from "react";
 import styled from "styled-components";
-import {FaTrash} from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { AiFillCheckCircle, AiOutlineCheckCircle } from "react-icons/ai";
 
-export default function WordList({WordList}){
-  return ( 
-  <WordListBlock>
-    {WordList.map((word)=> (
-    <WordItem word={word}/>
-    ))}
-  </WordListBlock>
+export default function WordList({ wordList, onRemove, onToggle }) {
+  return (
+    <WordListBlock>
+      {wordList.map((word) => (
+        <WordItem
+          key={word.id}
+          word={word}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </WordListBlock>
   );
 }
 
-function WordItem({word}) {
-  const [active,setActive] =useState(false);
+function WordItem({ word, onRemove, onToggle }) {
+  const [active, setActive] = useState(false);
   return (
-    <WordItemBlock onClickCapture={()=> setActive(!active)}>
-      <h3>{word.eng}</h3>
-      {active && <p>{word.kor}</p>}
+    <WordItemBlock onClick={() => setActive(!active)}>
+      <EngBox>
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+            onToggle(word.id);
+          }}
+        >
+          {word.active ? (
+            <AiFillCheckCircle size={24} />
+          ) : (
+            <AiOutlineCheckCircle size={24} />
+          )}
+        </div>
+        <h3>
+          {word.eng}
+          {/* 버블링 */}
+        </h3>
+        <FaTrash
+          onClick={(e) => {
+            // 이벤트 전파를 막는다.
+            e.stopPropagation();
+            onRemove(word.id);
+          }}
+        />
+      </EngBox>
+      {active && (
+        <p>
+          {word.kor.map((text, idx) => (
+            <div>
+              {idx + 1}. {text}
+            </div>
+          ))}
+        </p>
+      )}
     </WordItemBlock>
   );
 }
 
-const WordListBlock = styled.ul``;
-
-const WordItemBlock = styled.li`
-padding: 10px 20px;
-border-bottom: 1px solid #ddd;
-h3 {
-  display:flex;
-
-}
-cursor:pointer;
-user-select: none;
-p{
-border-top: 1px solid #ddd;
-margin-top: 10px;
-padding: 10px 0;
-}
+const WordListBlock = styled.ul`
+  overflow-y: auto;
+  flex: 1;
+  border-bottom: 1px solid #ddd;
 `;
 
+const WordItemBlock = styled.li`
+  padding: 10px 20px;
+  border-bottom: 1px solid #ddd;
+  cursor: pointer;
+  user-select: none;
+  p {
+    border-top: 1px solid #ddd;
+    margin-top: 10px;
+    padding: 10px 0;
+  }
+`;
 const EngBox = styled.div`
   display: flex;
   align-items: center;
   h3 {
     flex: 1;
-    margin-top:-7px;
-    margin-left:7px;
+    margin-top: -7px;
+    margin-left: 7px;
   }
-`
+`;
