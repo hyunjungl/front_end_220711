42c3df392dd767e3f247fa02942f90a362364073
hyunjungl <101260560+hyunjungl@users.noreply.github.com>
2022-08-15 00:51:36 +0900

    Update TodoBox.js

--- todo-list/src/components/TodoBox.js
+++ todo-list/src/components/TodoBox.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
+
+//App.js에 투두박스의 하위요소로 헤더랑 인풋을 넣었고, 임포트 했으므로 
+//제일 상위요소 안에 childeren 비구조 할당해서 넣어야지 자식요소들이 브라우저에 뜸
 export default function TodoBox({ children }) {
   return (
     <Container>
